eab1c484959a8749b5a0c8a3c96e467ba1dca0cf
shominees <jompa010@gmail.com>
2016-07-21 14:58:46 +0200

    added 2nd jsonUtil

--- src/main/java/utils/JsonUtils.java
+++ src/main/java/utils/JsonUtils.java
@@ -3,6 +3,7 @@ package utils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -17,8 +18,21 @@ public class JsonUtils {
     public static JsonNode stringToJson(String stringyfiedJson) {
         try {
             return new ObjectMapper().readTree(stringyfiedJson);
-        } catch (IOException ex) {
-            System.out.println(ex);
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @param jsonFile
+     * @return null if string not parsable
+     */
+    public static JsonNode jsonFromFile(File jsonFile) {
+        try {
+            return new ObjectMapper().readTree(jsonFile);
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
             return null;
         }
     }
